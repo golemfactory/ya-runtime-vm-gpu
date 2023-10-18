feca7c8dd1a1b1329795ddf2b251c3d0ddf06dda
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2023-10-18 05:26:32 +0200

    Finalize deb packaging

--- debian/build-deb.sh
+++ debian/build-deb.sh
@@ -5,23 +5,31 @@ set -e
 VERSION=$1
 SELF_TEST_IMG=$2
 
-tmp_dir=.debpkg/usr/lib/yagna/plugins/ya-runtime-vm-nvidia/runtime
+tmp_dir=.debpkg/usr/lib/yagna/plugins/ya-runtime-vm-nvidia
 
-mkdir -p ${tmp_dir}
+mkdir -p ${tmp_dir}/runtime
 mkdir -p .debpkg/DEBIAN
 
-cp "$SELF_TEST_IMG" "${tmp_dir}/self-test.gvmi"
-for file in bios-256k.bin efi-virtio.rom initramfs.cpio.gz kvmvapic.bin linuxboot_dma.bin vmlinuz-virt vmrt; do
-    ln -s ../../ya-runtime-vm/runtime/$file ${tmp_dir}/
+cp "$SELF_TEST_IMG" "${tmp_dir}/runtime/self-test.gvmi"
+for file in vgabios-stdvga.bin bios-256k.bin efi-virtio.rom kvmvapic.bin linuxboot_dma.bin vmrt; do
+    ln -s ../../ya-runtime-vm/runtime/$file ${tmp_dir}/runtime/
 done
+
+ln -s ../ya-runtime-vm/ya-runtime-vm ${tmp_dir}/ya-runtime-vm-nvidia
+cp conf/ya-runtime-vm-nvidia.json ${tmp_dir}.json
 
 cp debian/* .debpkg/DEBIAN/
 
 VERSION="$(echo "$VERSION" | sed -E "s,^refs/tags/,,")"
-FIXED_VERSION="$(echo "$VERSION" | sed -E 's/^v//')"
-sed -i "s/#version/${FIXED_VERSION}/g" .debpkg/DEBIAN/control
+if [[ "$VERSION" = "pre-rel-"* ]]; then
+    FIXED_VERSION="$(echo "$VERSION" | sed -E 's/^pre-rel-v//')"
+else
+    FIXED_VERSION="$(echo "$VERSION" | sed -E 's/^v//')"
+fi
+sed -i "s/#version/${FIXED_VERSION/-rc/~rc}/g" .debpkg/DEBIAN/control
+sed -i "s/__VERSION__/${FIXED_VERSION}/g" ${tmp_dir}.json
 
 DEB_FILE="ya-runtime-vm-nvidia_${VERSION}.deb"
 
-dpkg-deb --build .debpkg "$DEB_FILE"
+dpkg-deb --root-owner-group --build .debpkg "$DEB_FILE"
 echo "deb=$DEB_FILE" >> "${GITHUB_OUTPUT}"
